32f528a19c1473f494b7d766b9301f3ac78c7745
Lori Henderson <lorim95@gmail.com>
2022-05-17 08:57:20 -0400

    commit tree bf9af87748ab074f7a9ad3d5e78e23be22c61c1b

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+    return "This one is on me!"
+  } else if (400 < someValue  && someValue <= 2000) {
+    return "That will be twenty bucks."
+  } else if (2000 < someValue && someValue <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (someValue > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let checkCity = city==="NYC" ? "Ok, sounds good." : "No go."
+  return checkCity;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
